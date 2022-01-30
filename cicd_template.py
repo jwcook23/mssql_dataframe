c5a689e36b9302312913a952412a91dc31cb83e6
Jason Cook <jason.cook1989@gmail.com>
2022-01-30 13:37:12 -0500

    git tab build ver

--- cicd_template.py
+++ cicd_template.py
@@ -25,14 +25,16 @@ import subprocess
 import configparser
 import argparse
 import glob
+import re
 
 from conftest import options
 
 
-def run_cmd(cmd):
-    """Generic command line process and error if needed."""
-    # run all commands in virtual environment
-    cmd[0] = os.path.join(os.getcwd(), "env", "Scripts", cmd[0])
+def run_cmd(cmd, venv=True):
+    """Generic command line process and error if needed. Otherwise stdout is returned."""
+    # run all commands in virtual environment by default
+    if venv:
+        cmd[0] = os.path.join(os.getcwd(), "env", "Scripts", cmd[0])
     # call command line process
     status = subprocess.run(cmd, capture_output=True)
     if status.returncode != 0:
@@ -43,20 +45,22 @@ def run_cmd(cmd):
             msg = status.stdout.decode("utf-8")
         raise RuntimeError(msg)
 
+    return status.stdout.decode("utf-8")
+
 
 def run_black():
-    """black to auto-format code to standard."""
+    """Run black to auto-format code to standard."""
     print("running black for all Python files not excluded by .gitignore")
-    run_cmd(["black", "."])
+    _ = run_cmd(["black", "."])
 
 
 def run_flake8(config):
-    """flake8 to lint and check code quality"""
+    """Run flake8 to lint and check code quality."""
     try:
         print(
             "running flake8 for all Python files excluding virtual environment directory named 'env'"
         )
-        run_cmd(
+        _ = run_cmd(
             [
                 "flake8",
                 "--exclude=env",
@@ -70,7 +74,7 @@ def run_flake8(config):
 
 
 def run_coverage_pytest(config, args):
-    """pytest and coverage to ensure code works as desired and is covered by tests. Also produces test xml report for genbadge."""
+    """Run pytest and coverage to ensure code works as desired and is covered by tests. Also produces test xml report for genbadge."""
     print(f"running coverage for module: {config['metadata']['name']}")
     print(f"running tests for directory: {config['tool:pytest']['testpaths']}")
     # required arguments
@@ -88,27 +92,27 @@ def run_coverage_pytest(config, args):
     cmd += ["--" + k + "=" + v for k, v in args.items()]
 
     # use coverage to call pytest
-    run_cmd(cmd)
+    _ = run_cmd(cmd)
     print(f"generated coverage sqlite file: {config['coverage:run']['data_file']}")
     print(f"generated test xml file: {config['user:pytest']['junitxml']}")
 
 
 def coverage_html(config):
-    """coverage html report for user viewing"""
+    """Generage coverage html report for user viewing."""
     print(
         f"generating coverage html file: {os.path.join(config['coverage:html']['directory'], 'index.html')}"
     )
-    run_cmd(["coverage", "html"])
+    _ = run_cmd(["coverage", "html"])
 
 
 def coverage_xml(config):
-    """coverage xml report for genbadge"""
+    """Generate coverage xml report for genbadge."""
     print(f"generating coverage xml file: {config['coverage:xml']['output']}")
-    run_cmd(["coverage", "xml"])
+    _ = run_cmd(["coverage", "xml"])
 
 
 def generage_badges(config):
-    """generate badges using genbadge"""
+    """Generate badges using genbadge."""
     badges = {
         "tests": config["user:pytest"]["junitxml"],
         "coverage": config["coverage:xml"]["output"],
@@ -117,22 +121,40 @@ def generage_badges(config):
     for b, i in badges.items():
         fp = f"{config['genbadge']['output']}{b}.svg"
         print(f"generating badge for {b} at: {fp}")
-        run_cmd(["genbadge", b, "-i", i, "-o", fp])
+        _ = run_cmd(["genbadge", b, "-i", i, "-o", fp])
+
+
+def package_version():
+    "Set package version number in VERSION file using the latest git tag."
+
+    # read latest git tag
+    version = run_cmd(["git", "describe", "--tags", "--candidates=1"], venv=False)
+    version = version.strip()
+    print(f"Latest git tag version: {version}")
+
+    # use clean version
+    version = re.search(r"^v(\d+\.\d+\.\d+).*", version)
+    if version is None:
+        raise RuntimeError("Unable to clean git tag.")
+    version = version.group(1)
+    with open("VERSION", "w") as fh:
+        fh.write(version)
+    print(f"Version written to VERSION file: {version}")
 
 
 def build_package():
-    "build Python package for upload to PyPi.org"
+    "Build Python package."
 
     outdir = os.path.join(os.getcwd(), "dist")
     print(f"building package in directory: {outdir}")
 
     # build package .gz and .whl files
-    run_cmd(["python", "-m", "build", f"--outdir={outdir}"])
+    _ = run_cmd(["python", "-m", "build", f"--outdir={outdir}"])
     print(f"built source archive {glob.glob(os.path.join(outdir,'*.tar.gz'))}")
     print(f"built distribution {glob.glob(os.path.join(outdir,'*.whl'))}")
 
     # check build result
-    run_cmd(["twine", "check", os.path.join(outdir, "*")])
+    _ = run_cmd(["twine", "check", os.path.join(outdir, "*")])
 
 
 # parameters from setup.cfg
@@ -150,10 +172,12 @@ args = vars(args)
 # ignore None as would be passed as "None"
 args = {k: v for k, v in args.items() if v is not None}
 
+# TODO: pre-commit install
 run_black()
 run_flake8(config)
 run_coverage_pytest(config, args)
 coverage_html(config)
 coverage_xml(config)
 generage_badges(config)
+package_version()
 build_package()
--- mssql_dataframe/__init__.py
+++ mssql_dataframe/__init__.py
@@ -1,4 +1,4 @@
 from importlib.metadata import version
 from mssql_dataframe.package import SQLServer  # noqa: F401
 
-__version__ = version("mssql-dataframe")
+__version__ = version("mssql_dataframe")
